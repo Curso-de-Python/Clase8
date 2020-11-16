de92fc521f766311b69d8ee05e7d2100666feea3
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-11-16 14:13:38 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,29 @@
+# ALCANCES (SCOPES)
+
+def scopeTest():
+  x = 123
+
+scopeTest()
+print(x)
+'''
+# DETALLE SOBRE LISTAS
+
+lista1 = [0,1]
+lista2 = lista1
+#lista2[0] = 5
+#lista2 = [2,3]
+#lista2[0] = 10
+print("Lista1: ", lista1)
+print("Lista2: ", lista2)
+
+# INTERACCIÓN CON ARGUMENTOS
+
+def miFuncion(miLista1):
+  print(miLista1)
+  miLista1 = [0, 1]
+  #del miLista1[0]
+
+miLista2 = [2, 3]
+miFuncion(miLista2)
+print(miLista2)
+'''
